af92a2499097d2e81c779d74ea758b0ab424358f
yahya <yahyaelhakm@gmail.com>
2025-09-07 17:04:47 +0100

    edit detector.h detector.cpp main.cpp

--- include/analysis/detector.h
+++ include/analysis/detector.h
@@ -1,51 +1,6 @@
 #ifndef DETECTOR_H
 #define DETECTOR_H
 
-#include <string>
-#include <vector>
-#include <set>
-#include <chrono>
-#include <ctime>
-#include <iomanip>
 
-// Type du message détecté
-enum MessageType {
-    TEXT,
-    LINK,
-    MEDIA_OMITTED,
-    IMAGE,
-    VIDEO,
-    AUDIO,
-    DOCUMENT,
-    UNKNOWN
-};
-
-// Structure contenant les métadonnées d’un message WhatsApp
-struct Metadata {
-    std::string date;
-    std::string time;
-    std::string author;
-    std::string message;
-    MessageType type;
-};
-
-class MetaExtractor {
-public:
-    std::vector<Metadata> extract(const std::string& filepath);
-    void printAll(const std::vector<Metadata>& metas);
-    void exportToCSV(const std::vector<Metadata>& metas, const std::string& outputPath);
-    void logExport(const std::vector<Metadata>& metas, const std::string& outputPath);
-
-    std::set<std::string> loadSuspiciousWords(const std::string& filepath);
-    std::vector<Metadata> detectSuspiciousMessages(
-        const std::vector<Metadata>& metas,
-        const std::set<std::string>& suspiciousWords
-        );
-    void printSuspiciousMessages(const std::vector<Metadata>& flaggedMessages);
-
-private:
-    MessageType classify(const std::string& message);
-    std::string messageTypeToString(MessageType type);
-};
 
 #endif // DETECTOR_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,98 @@
-#include <iostream>
 #include "analysis/detector.h"
 #include "core/suspicious_words.h"
+
+#include <iostream>
 #include <string>
 #include <set>
 #include <vector>
-#include <filesystem>  // C++17
-#include <limits>
-#define NOMINMAX
 #include <windows.h>
+#include <commdlg.h>
+#include <cstdlib>
 
-void printMenu() {
-    std::cout << "===========================================\n";
-    std::cout << "      WhatsApp Metadata Analyzer\n";
-    std::cout << "===========================================\n";
-    std::cout << "1. Extraire et afficher tous les messages\n";
-    std::cout << "2. Détecter et afficher les messages suspects\n";
-    std::cout << "3. Exporter tous les messages vers CSV\n";
-    std::cout << "4. Exporter seulement les messages suspects vers CSV\n";
-    std::cout << "0. Quitter\n";
-    std::cout << "===========================================\n";
+void printMenu(int menuNumber, int& choice) {
+	int maxPossibleChoice = 0;
+    std::cout << "=======================================================\n";
+    std::cout << "        WhatsApp Detection Suspecious Messages\n";
+    std::cout << "=======================================================\n";
+    switch (menuNumber) {
+    case 1:
+        maxPossibleChoice = 2;
+        std::cout << "[1] Choisir le chemin vers le fichier zip\n";
+        std::cout << "[2] Regarder tutoriel pour extraire les messages\n";
+        std::cout << "    Whatsapp en anglais\n";
+        break;
+    case 5:
+        maxPossibleChoice = 4;
+        std::cout << "[1] Extraire et afficher tous les messages\n";
+        std::cout << "[2] Détecter et afficher les messages suspects\n";
+        std::cout << "[3] Exporter tous les messages vers CSV\n";
+        std::cout << "[4] Exporter seulement les messages suspects vers CSV\n";
+        break;
+    }
+    std::cout << "[0] Quitter\n";
+    std::cout << "=======================================================\n";
     std::cout << "Votre choix : ";
+    std::cin >> choice;
+    if (choice < 0 || choice > maxPossibleChoice) {
+        std::system("cls");
+		std::cout << "Choix invalide. Ressayez." << std::endl;
+		printMenu(menuNumber, choice);
+    }
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
 
-    MetaExtractor extractor;
-    std::string whatsappFile;
-    std::vector<Metadata> metas;
-    std::set<std::string> suspiciousWords;
+	int choice;
+    std::string zipPath;
 
-    std::cout << "Entrez le chemin du fichier WhatsApp exporte : \n";
-    std::getline(std::cin, whatsappFile);
 
-    metas = extractor.extract("C:\\Users\\hp7\\Desktop\\detection_suspecious_messages_in_whatsapp\\data\\WhatsApp Chat with.txt");
-    if (metas.empty()) {
-        std::cerr << "[!] Aucun message n'a été extrait. Vérifiez le fichier.\n";
-        return 1;
+    printMenu(1, choice);
+    if (choice == 1) {
+		std::cout << "Une fenêtre s'est ouverte, sélectionnez le fichier ZIP exporté depuis WhatsApp.\n\n";
+        // Structure OPENFILENAME pour configurer le dialogue
+        OPENFILENAME ofn;
+        char szFile[MAX_PATH] = { 0 };
+
+        ZeroMemory(&ofn, sizeof(ofn));
+        ofn.lStructSize = sizeof(ofn);
+        ofn.hwndOwner = NULL; // pas de fenêtre parent
+        ofn.lpstrFile = szFile;
+        ofn.nMaxFile = sizeof(szFile);
+        ofn.lpstrFilter = "ZIP Files\0*.zip\0All Files\0*.*\0";
+        ofn.nFilterIndex = 1;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+        if (GetOpenFileName(&ofn) == TRUE) {
+            zipPath = ofn.lpstrFile;
+            std::cout << "Fichier ZIP sélectionné : " << zipPath << std::endl;
+        }
+        else {
+            std::cout << "Aucun fichier sélectionné." << std::endl;
+            return 1;
+        }
     }
+    else if (choice == 2) {
+        std::cout << "\nTutoriel pour extraire les messages WhatsApp en anglais :\n";
+        std::cout << "1. Ouvrez WhatsApp sur votre téléphone.\n";
+        std::cout << "2. Allez dans Paramètres > Langue de l'application.\n";
+        std::cout << "3. Choisissez English (Important).\n";
+        std::cout << "4. Revenez à l'accueil.\n";
+        std::cout << "5. Entrez dans une conversation que vous voulez exporter.\n";
+        std::cout << "6. Cliquez sur les trois points en haut à droite > More > Export chat.\n";
+        std::cout << "7. Sélectionnez 'Without media' ou 'Include media'.\n";
+        std::cout << "8. Envoyez le fichier ZIP sur votre ordinateur.\n";
+        std::cout << "Pour plus de détails :\n";
+        std::cout << "youtube.com\n";
+		return 0;
 
-    std::string suspiciousFile = "C:\\Users\\hp7\\Desktop\\detection_suspecious_messages_in_whatsapp\\data\\suspicious_words_fr.txt";
-    if (!std::filesystem::exists(suspiciousFile)) {
-        std::cerr << "[!] Le fichier des mots suspects est introuvable : " << suspiciousFile << std::endl;
-        return 1;
-    }
-
-    suspiciousWords = extractor.loadSuspiciousWords(suspiciousFile);
-	//suspiciousWords = SuspiciousWords::frenchSuspiciousWords;
-    if (suspiciousWords.empty()) {
-        std::cerr << "[!] Aucun mot suspect chargé. La détection ne fonctionnera pas.\n";
     }
     else {
-        std::cout << "[+] " << suspiciousWords.size() << " mots suspects chargés depuis " << suspiciousFile << "\n";
+        return 0;
     }
 
-    if (!std::filesystem::exists("outputs")) std::filesystem::create_directory("outputs");
-    if (!std::filesystem::exists("logs")) std::filesystem::create_directory("logs");
 
-    int choice;
-    do {
-        printMenu();
-        std::cin >> choice;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        switch (choice) {
-        case 1:
-            std::cout << "\n[+] Affichage de tous les messages :\n";
-            extractor.printAll(metas);
-            break;
-
-        case 2: {
-            std::cout << "\n[!] Détection des messages suspects :\n";
-            auto flagged = extractor.detectSuspiciousMessages(metas, suspiciousWords);
-            extractor.printSuspiciousMessages(flagged);
-            break;
-        }
-
-        case 3: {
-            std::string outputFile = "outputs/all_messages.csv";
-            extractor.exportToCSV(metas, outputFile);
-            extractor.logExport(metas, outputFile);
-            break;
-        }
-
-        case 4: {
-            std::string outputFile = "outputs/suspicious_messages.csv";
-            auto flagged = extractor.detectSuspiciousMessages(metas, suspiciousWords);
-            extractor.exportToCSV(flagged, outputFile);
-            extractor.logExport(flagged, outputFile);
-            break;
-        }
-
-        case 0:
-            std::cout << "[+] Au revoir !\n";
-            break;
-
-        default:
-            std::cout << "[!] Choix invalide. Réessayez.\n";
-            break;
-        }
-        std::cout << std::endl;
-    } while (choice != 0);
 
     return 0;
 }
